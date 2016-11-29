f9194ffc2ead32a2291aef5030444e95f0221569
busywait <gh-public@aptile.co.uk>
2016-11-29 22:55:15 +0000

    Use common code

--- F-Number From Prompt.bat
+++ F-Number From Prompt.bat
@@ -1,20 +1,5 @@
 @echo OFF
 
-set /p fNumber="Enter the F-Number to tag this image with, 0 for unknown, or a blank line to do nothing and finish: "
-if .%fNumber%==. goto :doNothing
-::Let exiftool check that a valid number has been provided
-
-set "runner_dir=%~dp0"
-:: By default use exiftool to act on related sidecar files as well as the base files.
-set "runner=_exiftoolPlusSidecar.bat"
-:: If we've been passed a directory instead of a file list then exiftool will
-:: process every supported file. No need to look for sidecar files in this case.
-if EXIST "%~1\*" ( set "runner=_exiftool.bat" )
-
-call "%runner_dir%%runner%" "%runner_dir%_commandPrompInputOptions.txt" -FNumber=%fNumber% %*
-:: all done
-exit /b
-
-:doNothing
-:: User chose to do nothing
-@set /p a="No changes made. Press RETURN to close this window."
+set "tag=FNumber"
+set "userPrompt=F-Number, or 0 for unknown"
+call "%~dp0\Set From Prompt.bat" %*
