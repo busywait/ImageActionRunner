d1bfe49825e0ef48b34de8889522bdab399e6eaf
busywait <gh-public@aptile.co.uk>
2016-12-01 23:07:34 +0000

    Replace _exiftool.bat (used in Windows Send To) with "Send To Exiftool.bat"
    "Send To Exiftool" requires action to be given as an absolute path,
    _exiftool did not. "Send To Exiftool3.bat" has the old _exiftool
    behavioru.

--- _setupRunner.bat
+++ _setupRunner.bat
@@ -4,14 +4,31 @@
 @if [%runner_dir%]==[] ( set "mising_environment=runner_dir" )
 @if NOT []==[%missing_environment%] goto :missingEnv
 
+:: Only set the sidecar_mode if the calling script did not pass it in
+@if []==[%sidecar_mode%] call :setSidecarMode $*
+
+@if [%sidecar_mode%]==[ignore] (
+	set runner="exiftool.exe" -@ "%runner_dir%_interactiveOptions.txt" 
+	) else ( 
+	set runner="exiftool.exe" -execute -if "-e $filepath" -srcfile "%%d%%f.xmp" -@ "%runner_dir%_sidecarFormats.txt" -common_args -@ "%runner_dir%_interactiveOptions.txt" )
+@call :clearState
+@exit /b
+
+:setSidecarMode
 :: If we've been passed a directory instead of a file list then exiftool will
 :: process every writable file. No need to look for sidecar files in this case.
 :: Otherwise use exiftool to act on related sidecar files as well as the base files.
 @if EXIST "%~1\*" (
-	set runner="exiftool.exe" -@ "%runner_dir%_interactiveOptions.txt" 
+	set "sidecar_mode=ignore"
 	) else ( 
-	set runner="exiftool.exe" -execute -if "-e $filepath" -srcfile "%%d%%f.xmp" -@ "%runner_dir%_sidecarFormats.txt" -common_args -@ "%runner_dir%_interactiveOptions.txt" )
+	set "sidecar_mode=look" )
+exit /b
+	
+:clearState
+:: Allow running different scripts after each other in a command window
+@set sidecar_mode=
 @exit /b
 
 :missingEnv
+@call clearState
 @echo Missing environment variables: %missing_environment%
