9ceb8fdec717957452ec64cc7d3e68d6fd60a1ed
Steven <disp-gh@aptile.co.uk>
2017-08-14 22:42:54 +0100

    Use config file for HTML dump and allow action on multiple files at once...

--- EXIF/RawExifAsHtml.bat
+++ EXIF/RawExifAsHtml.bat
@@ -1,8 +1,15 @@
 @echo ON
 
 :: Creates a clever interactive hexidecimal (raw data) dump of image
-:: EXIF data and saves it to a file with the same name as the (first)
-:: image file given on the command line.
+:: EXIF data and saves it to a file with the same name as each
+:: image file given on the command line (or each file in the folder).
+
 @set "runner_dir=%~dp0"
-@call "%runner_dir%_callRunner.bat" -htmldump %1 > "%~n1.htm"
+:: Don't use output redirection, the -w parameter works better for this case because it 
+:: creates an output file for each input file instead of joining all output together.
+::@set "outfile=%~dpn1-exif.htm"
+@set "sidecar_mode=ignore"
+@set "action_params=-@ %runner_dir%_htmldump.txt"
+@call "%runner_dir%_callRunner.bat" %*
 @exit /b
+
