b74f6dfa8ed138be585a25165d3e09ac27988a98
Steven <disp-gh@aptile.co.uk>
2017-12-08 12:54:54 +0000

    Minor comment/whitespace change.

--- EXIF/_gpsCreateTrack.bat
+++ EXIF/_gpsCreateTrack.bat
@@ -17,9 +17,10 @@
 	set "folder=%~dp1"
 )
 @set "outfile=%folder%track.%gps_track_format%"
-:: Options are mostly in the _gps_create_track.txt argfile to avoid crazy % encoding
+:: Options are mostly in the _gps_create_track.txt argfile to avoid crazy % encoding, like this
 ::-if "$gpslongitude ge 0" -fileOrder DateTimeOriginal -d %%%%%%%%Y-%%%%%%%%m-%%%%%%%%dT%%%%%%%%H:%%%%%%%%M:%%%%%%%%SZ 
-::-p option kept on the command line here to allow format selection at run time and share code.
+
+::-p option is added to the command line here to allow format selection at run time (to share code):
 @set "action_params=-p "%runner_dir%_gps_%gps_track_format%.fmt" -@ %runner_dir%_gps_create_track.txt"
 :: .xmp files are scanned in a second pass which will write GPS files out of sequence, so ignore sidecars here
 @set "sidecar_mode=ignore"
