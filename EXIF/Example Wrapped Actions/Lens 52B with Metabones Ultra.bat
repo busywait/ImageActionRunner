ca56b73575148235be6c7c176673fba884a28d92
Steven <disp-gh@aptile.co.uk>
2017-12-14 13:06:41 +0000

    Changed "with" to "+" in the LensModel tag when using a converter to fit with LensFun convention, for example "Tamron SP 500mm f/8 Mirror 55BB + 2x converter 01F"

--- EXIF/Example Wrapped Actions/Lens 52B with Metabones Ultra.bat
+++ EXIF/Example Wrapped Actions/Lens 52B with Metabones Ultra.bat
@@ -1,6 +1,6 @@
 :: Run exiftool in a wrapper that can be used as an external editor in ACDSee.
 @echo ON
 @mode 190,1000
-@set "action=Lens 52B with Metabones Ultra"
+@set "action=Lens 52B with MSBU"
 @set runner_dir=%~dp0
 @call "%runner_dir%Exif Action Runner.bat" %*
